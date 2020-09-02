0e0e223f4bdb4637ea12bc15b4202da870cf65e3
Justin <justin.leung.ch@gmail.com>
2020-09-01 20:46:54 -0400

    Add CoordinateSpace2D

---
+++ app/src/main/java/com/leungjch/orbitalapp/helpers/CoordinateSpace2D.java
@@ -0,0 +1,11 @@
+package com.leungjch.orbitalapp.helpers;
+
+// Creates a 2D coordinate system that is used by the universe
+public class CoordinateSpace2D {
+    public CoordinateSpace2D(int universewidth, int universeheight, ) {
+
+    }
+
+
+
+}
